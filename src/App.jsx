208f696b1ecdeb99a4ad1ffff0c0c31e2772d60d
bmedi <90107662+bmedi@users.noreply.github.com>
2024-11-22 15:09:46 -0500

    Initial commit from your app

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+    import Button from '@mui/material/Button';
+
+    function App() {
+      return (
+        <div>
+          <h1>Welcome to the Automated File Organization Tool</h1>
+          <Button variant="contained" onClick={() => alert('Button clicked!')}>
+            Click Me
+          </Button>
+        </div>
+      );
+    }
+
+    export default App;
